66c4dbf18463e213285347e1859d1ffbdd8add0e
Jazz Lyles <ripley36706@gmail.com>
2016-08-08 22:28:56 -0700

    added title and screen width/height to Desktop launcher

--- desktop/src/com/existenzial/blackjack/desktop/DesktopLauncher.java
+++ desktop/src/com/existenzial/blackjack/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.existenzial.blackjack.Blackjack;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Blackjack";
+		config.width = 800;
+		config.height = 480;
+
 		new LwjglApplication(new Blackjack(), config);
 	}
 }
